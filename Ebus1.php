83d181e4b94724b89e601238452085ddb460abb5
Adam O Ceallaigh <117320641@umail.ucc.ie>
2018-02-27 17:38:06 +0000

    few changes to Ebus1.php,header and stuff

--- Ebus1.php
+++ Ebus1.php
@@ -3,7 +3,8 @@
         <head>
             <title>Select Product</title>
             
-            
+            <link href="https://fonts.googleapis.com/css?family=Calligraffitti|Delius|Give+You+Glory|La+Belle+Aurore|Oxygen+Mono|Schoolbell|Sirin+Stencil|Sue+Ellen+Francisco|The+Girl+Next+Door" rel="stylesheet">
+            <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
             <!--JQuery-->
             <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
             <script type= "text/javascript" src="calculation.js"></script>
@@ -13,11 +14,19 @@
         
         <body>
             
-                <ul>
-                  <li>Home</li>
-                  <li>About</li>
-                  <li>Shop</li>
-                </ul>
+                <div id="header">
+                      <div class="logo">
+                        <h2>Cloud 4 U</h2>
+                        <img src="https://ak8.picdn.net/shutterstock/videos/15371518/thumb/1.jpg">
+                      </div>
+                      
+                        <ul>
+                            <li><a href="homepage.html">My Website</a></li>
+                             <li><a href="Cloud4UAboutpage.html">About Us</a></li>
+                             <li><a href="#">About Cloud </a></li>
+                             <li><a href="Ebus1.php">Shop</a></li>
+                        </ul>
+                </div>
                 
                <section class="heading">
                    <h1>Select Product</h1>
@@ -65,8 +74,57 @@
                       <input type ="text" id="subtotal" name ="text" value="0.00" readonly>
                  </section>
                  </section>
-                 
-                 <div>
-                </div>           
-          </body>
-      </html>
+                 <div class="social">
+  <div class="images">
+    <a href = "#"><i class="fa fa-facebook fa-2x" aria-hidden="true" ></i></a>
+					<a href = "#"><i class="fa fa-twitter fa-2x" aria-hidden="true" ></i></a>
+					<a href = "#"><i class="fa fa-pinterest fa-2x" aria-hidden="true" ></i></a>
+					<a href = "#"><i class="fa fa-snapchat fa-2x" aria-hidden="true" ></i></a>
+					<a href = "#"><i class="fa fa-instagram fa-2x" aria-hidden="true" ></i></a>
+					<a href = "#"><i class="fa fa-linkedin fa-2x" aria-hidden="true" ></i></a>
+</div>
+</div>
+<div class="footer">
+  <div class="contact">
+    <div class="left1">
+     <div class="box1">
+      <h1>CONTACT US</h1>
+      <br>
+      <h4 class="location">We Located Near Downtown California</h4>
+       <br>
+      <h5>This is Our Official Site</h5>
+      <br>
+      <br>
+      <h4 class="address">2479 Winterville Terrace</h4>
+       <h4 class="address">Silicon Valley, CA 24073</h4>
+       <h4 class="address">Phone:&nbsp;&nbsp;<em class="number">872-930-526</em></h4>
+       <h4 class="address">Email Address:&nbsp;&nbsp;<em class="number">Cloud4U</em>@gmail.com</h4></h4>
+      </div> 
+    </div>
+    <div class="left2">
+      <div class="box2">
+      <h1>Have Any Queries?</h1>
+      <br>
+      <br>
+     
+      <i class="fa fa-pencil" aria-hidden="true"></i>
+        <h2>Write To Us</h2>
+        
+     <div class="form">
+      <form>
+        <strong>Name:</strong>&nbsp;&nbsp;<input type="text" name="usrname">
+</form>
+<br>
+<textarea rows="4" cols="50" name="comment" form="usrform">
+Enter text here...</textarea>
+       <br>
+       <input type="submit">
+        </div>
+      </div>
+    </div>
+  </div>
+ <p> Cloud 4 U, Copyright &copy; 2018 </p>
+    
+</div>
+  </body>
+</html>
